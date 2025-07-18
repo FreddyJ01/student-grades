ffb37f7994eb22483c0b8397302aaaadc1b637ff
FreddyJ01 <fahrodej01@gmail.com>
2025-07-18 13:41:45 -0500

    GPACalculator

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6ec9363d2cefe6c71df4cfd7ea51e68e352344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3975d9a22c6bbf8ad0154af9f6e6c98a0f51856c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
