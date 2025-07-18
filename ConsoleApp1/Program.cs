3975d9a22c6bbf8ad0154af9f6e6c98a0f51856c
FreddyJ01 <fahrodej01@gmail.com>
2025-07-18 13:34:36 -0500

    GPACalculatorMethod

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic; // This is to import the dictionary class.
 
 namespace ConsoleApp1;
 
@@ -11,7 +12,7 @@ class Program
         int[] andrewGrade = { 80, 83, 82, 88, 85 };
         int[] emmaGrade = { 84, 96, 73, 85, 79 };
         int[] loganGrade = { 90, 92, 98, 100, 97 };
-        
+
         // Student Grade Sum Calculation
         int sophiaSum = sophiaGrade.Sum();
         int andrewSum = andrewGrade.Sum();
@@ -27,10 +28,10 @@ class Program
         decimal loganAvg = (decimal)loganGrade.Average();
 
         // System.Console.WriteLine($"{sophiaAvg:f1} {andrewAvg:f1} {emmaAvg:f1} {loganAvg:f1}");
-        
+
         // Cumulative Grade Avg Array
         decimal[] collectiveAvg = [sophiaAvg, andrewAvg, emmaAvg, loganAvg];
-        
+
         // Command Line Display
         System.Console.WriteLine($@"
         Student        Grade
@@ -40,7 +41,8 @@ class Program
         Logan          {loganAvg:f1} {letterGrade(collectiveAvg[3])}
         ");
 
-
+        System.Console.WriteLine();
+        GPACalculator();
     }
    
     // Letter Grade Output Method
@@ -67,6 +69,47 @@ class Program
             return 'F';
         }
     }
-    
+    public static void GPACalculator()
+    {
+        // Creates my dictionary with key values pairs of course and the associated credit hours.
+        Dictionary<string, decimal> courseCreditHours = new Dictionary<string, decimal>();
+        courseCreditHours.Add("English 101", 3.0m);
+        courseCreditHours.Add("Algebra 101", 3.0m);
+        courseCreditHours.Add("Biology 101", 4.0m);
+        courseCreditHours.Add("Computer Science I", 4.0m);
+        courseCreditHours.Add("Psychology 101", 3.0m);
+
+
+        // Creates my dictionary with key value pairs of sophias course grades.
+        Dictionary<string, decimal> sophiaCourseGrade = new Dictionary<string, decimal>();
+        sophiaCourseGrade.Add("English 101", 4.0m);
+        sophiaCourseGrade.Add("Algebra 101", 3.0m);
+        sophiaCourseGrade.Add("Biology 101", 3.0m);
+        sophiaCourseGrade.Add("Computer Science I", 3.0m);
+        sophiaCourseGrade.Add("Psychology 101", 4.0m);
+
+        decimal GPACalculator = (
+            ((courseCreditHours["English 101"] * sophiaCourseGrade["English 101"]) +
+            (courseCreditHours["Algebra 101"] * sophiaCourseGrade["Algebra 101"]) +
+            (courseCreditHours["Biology 101"] * sophiaCourseGrade["Biology 101"]) +
+            (courseCreditHours["Computer Science I"] * sophiaCourseGrade["Computer Science I"]) +
+            (courseCreditHours["Psychology 101"] * sophiaCourseGrade["Psychology 101"])) /
+            ((courseCreditHours["English 101"] + courseCreditHours["Algebra 101"] + courseCreditHours["Biology 101"] + courseCreditHours["Computer Science I"] + courseCreditHours["Psychology 101"]))
+            );
+        
+        
+        System.Console.WriteLine(@$"
+        Student: Sophia Johnson
+
+        Course               Grade            Credit Hours
+        English 101          {sophiaCourseGrade["English 101"]:f0}                {courseCreditHours["English 101"]:f0}
+        Algebra 101          {sophiaCourseGrade["Algebra 101"]:f0}                {courseCreditHours["Algebra 101"]:f0}
+        Biology 101          {sophiaCourseGrade["Biology 101"]:f0}                {courseCreditHours["Biology 101"]:f0}
+        Computer Science I   {sophiaCourseGrade["Computer Science I"]:f0}                {courseCreditHours["Computer Science I"]:f0}
+        Psychology 101       {courseCreditHours["Psychology 101"]:f0}                {courseCreditHours["Psychology 101"]:f0}
+        
+        Final GPA:           {GPACalculator:f2}
+        ");
+    }
 
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6ec9363d2cefe6c71df4cfd7ea51e68e352344")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
